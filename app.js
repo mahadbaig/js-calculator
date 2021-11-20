7befc815feadbf08dced9195cd7830d9349195fe
Mahad <mahadmirza681@gmail.com>
2021-11-20 13:23:18 +0500

    Calculator updated

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 function buttontap(num) {
-    var button = document.getElementById("screen")
+    var button = document.getElementById("screen");
     button.value += num
 }
 
 function results() {
     var result = document.getElementById("screen");
     var finalresult = eval(result.value)
+    result.value = finalresult
 }
 
-function clear() {
+function oneClear(){
     var clear = document.getElementById("screen")
-    clear.value = ""
+
+    // console.log(input.value.slice(0 , -1))
+
+    clear.value = clear.value.slice(0 , -1)
+
+}
+
+function allClear(){
+    var allclear = document.getElementById("screen")
+    allclear.value = ""   
 }
